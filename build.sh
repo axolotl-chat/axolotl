38bfae250e84596c17493d160a9c25fef52a415e
Jani Monoses <jani@ubuntu.com>
2015-09-03 01:06:33 +0300

    Delete old clicks when building.

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ echo "Building package in $mode mode."
 
 rm -Rf builddir
 
+rm textsecure.jani*click
+
 cp -a click builddir
 
 docker run --rm -it -v $(pwd):/home/developer -v $GOPATH:/home/developer/gopath -w $(pwd|sed "s,$GOPATH,/home/developer/gopath,") janimo/goqml-cross build -i -o builddir/textsecure
