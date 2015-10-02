a7b453ed244bdddb83a64b5c90af07f8918e09e8
Jani Monoses <jani@ubuntu.com>
2015-10-02 11:34:47 +0300

    Add Changelog to click package.

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ cp -a click builddir
 docker run --rm -it -v $(pwd):/home/developer -v $GOPATH:/home/developer/gopath -w $(pwd|sed "s,$GOPATH,/home/developer/gopath,") janimo/goqml-cross build -i -o builddir/textsecure
 mkdir -p builddir/qml
 cp -a qml/phoneui builddir/qml
+cp -a CHANGELOG.md builddir
 if [ $mode = "dev" ];then
 	#copy config.yml or rootCA.pem
 	cp -a dev/* builddir/
