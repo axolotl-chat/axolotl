14b278d8c1f30302701aca74d0fee91f026e3e3f
Aaron Kimmig <aaron@convive.io>
2019-10-13 21:56:58 +0200

    add introduction

--- axolotl-web/src/pages/Register.vue
+++ axolotl-web/src/pages/Register.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="register">
-    <VuePhoneNumberInput v-model="phone" @update="updatePhone" :callingCode="cc" class="phoneInput" />
-    <button class="btn btn-primary" @click="requestCode()"> request code </button>
+    <div v-if="infoPage" class="page1 info">
+      <img class="logo" src="/axolotl.png" />
+      <h1 class="title">Axolotl Alpha</h1>
+      <h2 class="subtitle">A cross-plattform signal client</h2>
+      <div class="description">Hey! Mr. Tambourine Man, play a song for me,
+        <br />
+        In the jingle jangle morning I'll come following you.
+        <br />
+        <a href="https://axolotl.chat">https://axolotl.chat</a>
+        <br />
+        <font-awesome-icon id="heart" icon="heart" />
+      </div>
+      <button class="btn btn-primary" @click="infoPage=false">Next</button>
+    </div>
+    <div class="registration">
+      <VuePhoneNumberInput v-model="phone" @update="updatePhone" :callingCode="cc" class="phoneInput" />
+      <button class="btn btn-primary" @click="requestCode()"> request code </button>
+    </div>
   </div>
 </template>
 
@@ -29,15 +45,33 @@ export default {
   data() {
     return {
       phone: '',
-      cc:""
+      cc:"",
+      infoPage:true,
     };
   },
 }
 </script>
 <style scoped>
+  .info,
   .register{
     display:flex;
     flex-direction: column;
+  }
+  .info{
+    position:fixed;
+    width:100vw;
+    height:100vh;
+    background-color:#FFF;
+    top:0px;
+    left:0px;
+    z-index:12;
+    text-align:center;
+  }
+  h1{
+    font-size:1.5rem;
+  }
+  h2{
+    font-size:1.3rem;
   }
   .phoneInput{
     margin-top:30px;
@@ -47,6 +81,14 @@ export default {
     margin: auto;
     margin-top: auto;
     margin-top: 50px;
+  }
+  .logo{
+    margin: 20px auto;
+    border-radius: 10px;
+  }
+  #heart{
+    font-size: 2rem;
+    color:#2090ea;
   }
 </style>
 
