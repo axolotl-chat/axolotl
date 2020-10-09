22eff4ecb4450b22f59760a34f66754f614b14bd
Olof Nord <olof.nord@tutanota.com>
2020-10-09 22:48:09 +0200

    Add initial AppImage packaging
    This requires appimagetool to be installed locally.
    
    It does not yet work as the location of the axolotl-web is hardcoded.
    Once the Flatpak packaging PR is merged the following parameter should be added to the .desktop file:
    
     -axolotlWebDir=usr/bin/axolotl-web/dist

---
+++ appimage/build.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# https://sipb.mit.edu/doc/safe-shell/
+set -eu -o pipefail
+
+for build_dependency in go npm appimagetool
+do
+  if [ ! -f "$(command -v "${build_dependency}")" ]; then
+    echo "${dependency} is required!"
+    echo "Please install all build dependencies first."
+    exit 1
+  fi
+done
+
+echo "build axolotl"
+pushd ../
+  go clean
+  go mod download -x
+  go build -v
+popd
+
+echo "build axolotl-web"
+pushd ../axolotl-web
+  npm install
+  npm run build
+popd
+
+echo "packaging it all up"
+pushd ../
+  rm -rf build/AppDir
+
+  mkdir -p build/AppDir/usr/bin
+  cp -f axolotl build/AppDir/usr/bin/axolotl
+
+  mkdir -p build/AppDir/usr/bin/axolotl-web
+  cp -rf axolotl-web/dist build/AppDir/usr/bin/axolotl-web
+
+  cp -f appimage/AppDir/AppRun build/AppDir/AppRun
+  cp -f appimage/AppDir/axolotl.desktop build/AppDir/axolotl.desktop
+  cp -f appimage/AppDir/axolotl.png build/AppDir/axolotl.png
+
+  appimagetool build/AppDir
+popd
