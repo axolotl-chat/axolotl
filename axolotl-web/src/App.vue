550d2096fe73c138b20f3d61ec54847c9a6d88d1
aaron@nanu-c.org <aaron@nan-c.org>
2021-03-21 10:42:00 +0100

    use let instead of var and === instead  of ==

--- axolotl-web/src/App.vue
+++ axolotl-web/src/App.vue
@@ -10,11 +10,11 @@
 
 <script>
 window.getCookie = function (cname) {
-  var name = cname + "=";
-  var ca = document.cookie.split(";");
-  for (var i = 0; i < ca.length; i++) {
+  let name = cname + "=";
+  let ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
     var c = ca[i];
-    while (c.charAt(0) == " ") {
+    while (c.charAt(0) === " ") {
       c = c.substring(1);
     }
     if (c.indexOf(name) == 0) {
