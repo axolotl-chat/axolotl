8dee852edb14bbaa7e74384a04f106dfa335138a
aaron@nanu-c.org <aaron@nan-c.org>
2021-04-23 00:05:34 +0200

    make linter happy

--- axolotl-web/src/pages/Register.vue
+++ axolotl-web/src/pages/Register.vue
@@ -2,41 +2,42 @@
   <div class="register">
     <WarningMessage />
     <div v-if="infoPage" class="page1 info">
-      <img class="logo" src="/axolotl.png" />
+      <img class="logo" src="/axolotl.png">
       <h1 class="title">Axolotl Beta</h1>
-      <h2 class="subtitle" v-translate>A cross-plattform signal client</h2>
+      <h2 v-translate class="subtitle">A cross-plattform signal client</h2>
       <div class="description">
         Hey! Mr. Tambourine Man, play a song for me,
-        <br />
+        <br>
         In the jingle jangle morning I'll come following you.
-        <br />
+        <br>
         It's beta, expect lot's of things not working.
-        <br />
+        <br>
         <a
           href="https://axolotl.chat"
           @click="openExtern($event, 'https://axolotl.chat')"
-          >https://axolotl.chat</a
         >
-        <br />
+          https://axolotl.chat
+        </a>
+        <br>
         <font-awesome-icon id="heart" icon="heart" />
       </div>
-      <button class="btn btn-primary" @click="infoPage = false" v-translate>
+      <button v-translate class="btn btn-primary" @click="infoPage = false">
         Next
       </button>
     </div>
-    <div class="rateLimit-error" v-if="ratelimitError != null">
+    <div v-if="ratelimitError != null" class="rateLimit-error">
       <div class="error">
         {{ ratelimitError }}
       </div>
     </div>
     <div v-else class="registration">
       <VueTelInput
-        @validate="updatePhone"
+        id="phoneInput"
         mode="international"
         class="phoneInput"
-        id="phoneInput"
+        @validate="updatePhone"
       />
-      <button class="btn btn-primary" @click="requestCode()" v-translate>
+      <button v-translate class="btn btn-primary" @click="requestCode()">
         Request code
       </button>
     </div>
@@ -50,10 +51,30 @@ import WarningMessage from "@/components/WarningMessage";
 import { mapState } from "vuex";
 
 export default {
-  name: "register",
+  name: "RegisterPage",
   components: {
     VueTelInput,
     WarningMessage,
+  },
+  data() {
+    return {
+      phone: "",
+      infoPage: true,
+    };
+  },
+  computed: mapState([
+    "gui",
+    "ratelimitError",
+    "registrationStatus",
+    "captchaToken",
+    "captchaTokenSent",
+  ]),
+  mounted() {
+    var userLang = navigator.language || navigator.userLanguage;
+    this.$language.current = userLang;
+    if (this.captchaToken != null && !this.captchaTokenSent) {
+      this.$store.dispatch("sendCaptchaToken");
+    }
   },
   methods: {
     requestCode() {
@@ -69,27 +90,6 @@ export default {
       }
     },
   },
-  mounted() {
-    var userLang = navigator.language || navigator.userLanguage;
-    this.$language.current = userLang;
-    if (this.captchaToken != null && !this.captchaTokenSent) {
-      this.$store.dispatch("sendCaptchaToken");
-    }
-  },
-  data() {
-    return {
-      phone: "",
-      infoPage: true,
-    };
-  },
-  computed: mapState([
-    "gui",
-    "ratelimitError",
-    "registrationStatus",
-    "captchaToken",
-    "captchaTokenSent",
-  ]),
-  watch: {},
 };
 </script>
 <style scoped>
