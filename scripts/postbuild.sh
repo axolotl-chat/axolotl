b26be0533e7b69f3d4c2b01f98d4b1f8768d7445
aaron@nanu-c.org <aaron@nan-c.org>
2021-07-04 15:07:23 +0200

    put zkgroup logic for clickable in the postbuild script

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -7,3 +7,17 @@ cp -a ../../../click/* $@
 # Build axolotl-web
 echo "update translations and build axolotl-web $@"
 cd ../../../axolotl-web&&npm run translate &&npm run build && mkdir $@/axolotl-web&&cp dist $@/axolotl-web/ -r
+
+echo $ARCH
+if [ $ARCH == "arm64" ]
+then
+    [ -z '$GOPATH' ] && cp /github/workspace/go/src/github.com/nanu-c/zkgroup/lib/libzkgroup_linux_aarch64.so $@/lib/libzkgroup_linux_aarch64.so; 
+    cp $GOPATH/src/github.com/nanu-c/zkgroup/lib/libzkgroup_linux_aarch64.so $@/lib/libzkgroup_linux_aarch64.so ||true; 
+    rm ${INSTALL_DIR}/\\$GITHUB_WORKSPACE||true
+fi
+if [ $ARCH == "armhf" ]
+then
+    [ -z '$GOPATH' ] && cp /github/workspace/go/src/github.com/nanu-c/zkgroup/lib/libzkgroup_linux_armhf.so $@/lib/libzkgroup_linux_armhf.so; 
+    cp $GOPATH/src/github.com/nanu-c/zkgroup/lib/libzkgroup_linux_armhf.so $@/lib/libzkgroup_linux_armhf.so ||true; 
+    rm ${INSTALL_DIR}/\\$GITHUB_WORKSPACE||true
+fi
