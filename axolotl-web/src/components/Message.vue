20e1cc328f7f0b7bd3c78a2cd5ecfafdc2d7ece0
Olof <olof.nord@tutanota.com>
2021-07-13 19:01:52 +0200

    axolotl-web: Revise Message linter messages (#526)
    * Specify Message component prop types and add default values
    * Clarified comment in component following MR discussion

--- axolotl-web/src/components/Message.vue
+++ axolotl-web/src/components/Message.vue
@@ -134,8 +134,8 @@
         <div
           class="message-text-content"
           v-html="linkify(sanitize(message.Message))"
-        ></div>
-        <div v-translate v-if="message.Flags===17">Group changed.</div>
+        />
+        <div v-if="message.Flags===17" v-translate>Group changed.</div>
         <div
           v-if="
             message.Attachment.includes('null') &&
@@ -202,7 +202,20 @@ let decoder;
 
 export default {
   name: "Message",
-  props: ["message", "isGroup", "names"],
+  props: {
+    message: {
+      type: Object,
+      default: () => {}
+    },
+    isGroup: {
+      type: Boolean,
+      default: false,
+    },
+    names: {
+      type: Array,
+      default: () => []
+    }
+  },
   data() {
     return {
       showDate: false,
@@ -215,7 +228,8 @@ export default {
         !this.message.Outgoing &&
         this.isGroup &&
         (this.message.Flags === 0 || this.message.Flags === 14)
-      ); // 14 is quoting messages
+      ); // #14 is the flag for quoting messages
+	    // see this list for all message types: https://github.com/nanu-c/axolotl/blob/main/app/helpers/models.go#L15
     },
   },
   methods: {
