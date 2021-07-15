1471c04df08b5f1ebb2523041d68ae5a9294a17d
aaron@nanu-c.org <aaron@nan-c.org>
2021-07-15 22:01:52 +0200

    revise review

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -11,13 +11,13 @@ cd ../../../axolotl-web&&npm run translate &&npm run build && mkdir $@/axolotl-w
 echo $ARCH
 if [ $ARCH == "arm64" ]
 then
-    [ -z '$GOPATH' ] && cp /github/workspace/go/src/github.com/nanu-c/zkgroup/lib/libzkgroup_linux_aarch64.so $@/lib/libzkgroup_linux_aarch64.so; 
-    cp $GOPATH/src/github.com/nanu-c/zkgroup/lib/libzkgroup_linux_aarch64.so $@/lib/libzkgroup_linux_aarch64.so ||true; 
-    rm ${INSTALL_DIR}/\\$GITHUB_WORKSPACE||true
+    [ -z '$GOPATH' ] && cp /github/workspace/go/src/github.com/nanu-c/zkgroup/lib/libzkgroup_linux_aarch64.so $@/lib/libzkgroup_linux_aarch64.so;
+    cp $GOPATH/src/github.com/nanu-c/zkgroup/lib/libzkgroup_linux_aarch64.so $@/lib/libzkgroup_linux_aarch64.so ||true;
+    rm -f ${INSTALL_DIR}/\\${GITHUB_WORKSPACE}
 fi
 if [ $ARCH == "armhf" ]
 then
-    [ -z '$GOPATH' ] && cp /github/workspace/go/src/github.com/nanu-c/zkgroup/lib/libzkgroup_linux_armv7.so $@/lib/libzkgroup_linux_armv7.so; 
-    cp $GOPATH/src/github.com/nanu-c/zkgroup/lib/libzkgroup_linux_armv7.so $@/lib/libzkgroup_linux_armv7.so ||true; 
-    rm ${INSTALL_DIR}/\\$GITHUB_WORKSPACE||true
+    [ -z '$GOPATH' ] && cp /github/workspace/go/src/github.com/nanu-c/zkgroup/lib/libzkgroup_linux_armv7.so $@/lib/libzkgroup_linux_armv7.so;
+    cp $GOPATH/src/github.com/nanu-c/zkgroup/lib/libzkgroup_linux_armv7.so $@/lib/libzkgroup_linux_armv7.so ||true;
+    rm -f ${INSTALL_DIR}/\\${GITHUB_WORKSPACE}
 fi
