b5623109545642e3083c1d8c18d0a418d70ba655
Jonatan Hatakeyama Zeidler <jonatan_zeidler@gmx.de>
2021-09-12 22:39:36 +0200

    Install libzkgroup_linux_*.so file also on amd64 and with alternative path

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# make sure the script fails on errors
+set -Eeuo pipefail
+
 # copy click files
 echo "copy click files $@"
 
@@ -8,16 +11,40 @@ cp -a ../../../click/* $@
 echo "update translations and build axolotl-web $@"
 cd ../../../axolotl-web&&npm run translate &&npm run build && mkdir $@/axolotl-web&&cp dist $@/axolotl-web/ -r
 
-echo $ARCH
-if [ $ARCH == "arm64" ]
+case $ARCH in
+	amd64)
+		ARCH_NAME=x86_64
+		;;
+	arm64)
+		ARCH_NAME=aarch64
+		;;
+	armhf)
+		ARCH_NAME=armv7
+		;;
+esac
+
+readonly FILENAME=libzkgroup_linux_$ARCH_NAME.so
+
+if [ -v $GOPATH ]
 then
-    [ -z '$GOPATH' ] && cp /github/workspace/go/src/github.com/nanu-c/zkgroup/lib/libzkgroup_linux_aarch64.so $@/lib/libzkgroup_linux_aarch64.so;
-    cp $GOPATH/src/github.com/nanu-c/zkgroup/lib/libzkgroup_linux_aarch64.so $@/lib/libzkgroup_linux_aarch64.so ||true;
-    rm -f ${INSTALL_DIR}/\\${GITHUB_WORKSPACE}
-fi
-if [ $ARCH == "armhf" ]
-then
-    [ -z '$GOPATH' ] && cp /github/workspace/go/src/github.com/nanu-c/zkgroup/lib/libzkgroup_linux_armv7.so $@/lib/libzkgroup_linux_armv7.so;
-    cp $GOPATH/src/github.com/nanu-c/zkgroup/lib/libzkgroup_linux_armv7.so $@/lib/libzkgroup_linux_armv7.so ||true;
-    rm -f ${INSTALL_DIR}/\\${GITHUB_WORKSPACE}
+	# Github
+	readonly ZKGROUP_GITHUB=/github/workspace/go/src/github.com/nanu-c/zkgroup/lib/$FILENAME
+
+	cp $ZKGROUP_GITHUB $@/lib/;
+	rm -f ${INSTALL_DIR}/\\${GITHUB_WORKSPACE}
+else
+	# Clickable
+	readonly ZKGROUP_OPTION1=$GOPATH/pkg/mod/github.com/nanu-c/zkgroup@v0.8.8/lib/$FILENAME
+	readonly ZKGROUP_OPTION2=$GOPATH/src/github.com/nanu-c/zkgroup/lib/$FILENAME
+
+	mkdir -p $CLICK_LD_LIBRARY_PATH
+
+	[ -f $ZKGROUP_OPTION1 ] && cp $ZKGROUP_OPTION1 $CLICK_LD_LIBRARY_PATH/;
+	[ -f $ZKGROUP_OPTION2 ] && cp $ZKGROUP_OPTION2 $CLICK_LD_LIBRARY_PATH/;
+
+	if [ ! -f $CLICK_LD_LIBRARY_PATH/$FILENAME ]
+	then
+		echo "Didn't find $FILENAME which is required"
+		exit 1
+	fi
 fi
