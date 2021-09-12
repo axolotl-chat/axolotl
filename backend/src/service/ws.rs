934ff02b997b03ca8612b61662f7c3f644ce245a
Jonatan Hatakeyama Zeidler <jonatan_zeidler@gmx.de>
2021-09-12 23:21:24 +0200

    start on implementing protocol

--- backend/src/service/ws.rs
+++ backend/src/service/ws.rs
@@ -1,9 +1,11 @@
+use crate::requests::registration;
 use crate::requests::Request;
 use crate::requests::Result;
 use crate::service::RequestSender;
 use futures::SinkExt;
 use futures::StreamExt;
 use libsignal_service::provisioning::VerificationCodeResponse;
+use serde::Deserialize;
 use serde::Serialize;
 use tokio::sync::mpsc;
 use tokio::sync::oneshot;
@@ -51,67 +53,139 @@ async fn handle_message(msg: Message, tx: &mpsc::Sender<Request>) -> Result<Mess
         .to_str()
         .map_err(|_| "Message is not text. Failed to parse.".to_string())?;
 
-    let register = serde_json::from_str(content);
-    let confirm = serde_json::from_str(content);
+    let type_check: RequestMessage<TypeCheck> =
+        serde_json::from_str(content).map_err(|e| e.to_string())?;
 
-    if let Ok(data) = register {
-        let (cb_tx, cb_rx) = oneshot::channel();
-        tx.send(Request::Register(data, cb_tx))
-            .await
-            .map_err(|e| e.to_string())?;
+    if type_check.type_t != CRYFISH_WEBSOCKET_TYPE_REQUEST {
+        return Err("Received Message is no valid request".to_string());
+    }
 
-        match cb_rx
-            .await
-            .map_err(|e| e.to_string())?
-            .map_err(|e| e.to_string())?
-        {
-            VerificationCodeResponse::Issued => {
-                println!("Registration request was sent");
-                return Ok(Message::text(
-                    serde_json::to_string(&Success { success: true }).map_err(|e| e.to_string())?,
-                ));
-            }
-            VerificationCodeResponse::CaptchaRequired => {
-                println!("Server requires a Captcha. Please generate one and try again!");
-                return Ok(Message::text(
-                    serde_json::to_string(&Error {
-                        error: "Captcha required".to_string(),
-                    })
-                    .map_err(|e| e.to_string())?,
-                ));
-            }
+    match type_check.request.type_t {
+        CRYFISH_WEBSOCKET_MESSAGE_REGISTRATION => handle_register(content, tx).await,
+        CRYFISH_WEBSOCKET_MESSAGE_CONFIRM_REGISTRAION => handle_register_confirm(content, tx).await,
+        _ => Err("Received request type is invalid".to_string()),
+    }
+}
+
+async fn handle_register(content: &str, tx: &mpsc::Sender<Request>) -> Result<Message> {
+    println!("Handle registration message");
+    let req: NestedRequest<registration::Register> =
+        serde_json::from_str(content).map_err(|e| e.to_string())?;
+
+    let (cb_tx, cb_rx) = oneshot::channel();
+    tx.send(Request::Register(req.request.message, cb_tx))
+        .await
+        .map_err(|e| e.to_string())?;
+
+    match cb_rx
+        .await
+        .map_err(|e| e.to_string())?
+        .map_err(|e| e.to_string())?
+    {
+        VerificationCodeResponse::Issued => {
+            println!("Registration request was sent");
+            Ok(Message::text(
+                serde_json::to_string(&NestedResponse::new(
+                    Success { success: true },
+                    CRYFISH_WEBSOCKET_MESSAGE_REGISTRATION,
+                ))
+                .map_err(|e| e.to_string())?,
+            ))
         }
-    };
+        VerificationCodeResponse::CaptchaRequired => {
+            println!("Server requires a Captcha. Please generate one and try again!");
+            Ok(Message::text(
+                serde_json::to_string(&NestedResponse::new(
+                    Error {
+                        error: "Captcha required".to_string(),
+                    },
+                    CRYFISH_WEBSOCKET_MESSAGE_REGISTRATION,
+                ))
+                .map_err(|e| e.to_string())?,
+            ))
+        }
+    }
+}
 
-    if let Ok(data) = confirm {
-        let (cb_tx, cb_rx) = oneshot::channel();
-        tx.send(Request::ConfirmRegistration(data, cb_tx))
-            .await
-            .map_err(|e| e.to_string())?;
+async fn handle_register_confirm(content: &str, tx: &mpsc::Sender<Request>) -> Result<Message> {
+    println!("Handle registration confirmation message");
+    let req: NestedRequest<registration::ConfirmRegistration> =
+        serde_json::from_str(content).map_err(|e| e.to_string())?;
 
-        let response_data = cb_rx
-            .await
-            .map_err(|e| e.to_string())?
-            .map_err(|e| e.to_string())?;
+    let (cb_tx, cb_rx) = oneshot::channel();
+    tx.send(Request::ConfirmRegistration(req.request.message, cb_tx))
+        .await
+        .map_err(|e| e.to_string())?;
 
-        return Ok(Message::text(
-            serde_json::to_string(&RegistrationData {
-                uuid: response_data.uuid.as_u128(),
-                storage_capable: response_data.storage_capable,
-            })
-            .map_err(|e| e.to_string())?,
-        ));
-    };
+    let response_data = cb_rx
+        .await
+        .map_err(|e| e.to_string())?
+        .map_err(|e| e.to_string())?;
 
     Ok(Message::text(
-        serde_json::to_string(&Error {
-            error: format!(
-                "The message could not be parsed as any known type: {:?}, {:?}",
-                register, confirm
-            ),
-        })
-        .map_err(|_| "Response could not be parsed")?,
+        serde_json::to_string(&NestedResponse::new(
+            RegistrationData {
+                uuid: response_data.uuid.as_u128(),
+                storage_capable: response_data.storage_capable,
+            },
+            CRYFISH_WEBSOCKET_MESSAGE_CONFIRM_REGISTRAION,
+        ))
+        .map_err(|e| e.to_string())?,
     ))
+}
+
+const CRYFISH_WEBSOCKET_TYPE_UNKNOWN: u32 = 0;
+const CRYFISH_WEBSOCKET_TYPE_REQUEST: u32 = 1;
+const CRYFISH_WEBSOCKET_TYPE_RESPONSE: u32 = 2;
+
+const CRYFISH_WEBSOCKET_MESSAGE_UNKNOWN: u32 = 0;
+const CRYFISH_WEBSOCKET_MESSAGE_REGISTRATION: u32 = 1;
+const CRYFISH_WEBSOCKET_MESSAGE_CONFIRM_REGISTRAION: u32 = 2;
+
+impl<T> NestedResponse<T> {
+    fn new(message: T, type_t: u32) -> Self {
+        NestedResponse {
+            type_t: CRYFISH_WEBSOCKET_TYPE_RESPONSE,
+            response: ResponseBody { type_t, message },
+        }
+    }
+}
+
+type NestedRequest<T> = RequestMessage<RequestBody<T>>;
+type NestedResponse<T> = ResponseMessage<ResponseBody<T>>;
+
+#[derive(Deserialize)]
+struct RequestMessage<T> {
+    #[serde(rename = "type")]
+    type_t: u32,
+    request: T,
+}
+
+#[derive(Deserialize)]
+struct TypeCheck {
+    #[serde(rename = "type")]
+    type_t: u32,
+}
+
+#[derive(Deserialize)]
+struct RequestBody<T> {
+    #[serde(rename = "type")]
+    type_t: u32,
+    message: T,
+}
+
+#[derive(Serialize)]
+struct ResponseMessage<T> {
+    #[serde(rename = "type")]
+    type_t: u32,
+    response: T,
+}
+
+#[derive(Serialize)]
+struct ResponseBody<T> {
+    #[serde(rename = "type")]
+    type_t: u32,
+    message: T,
 }
 
 #[derive(Serialize)]
