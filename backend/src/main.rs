d63e87fa58e50a5ea9c36acd2554a5e84e7891c3
Jonatan Hatakeyama Zeidler <jonatan_zeidler@gmx.de>
2021-09-12 10:07:59 +0200

    Add json parser to websocket

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -9,13 +9,12 @@ use phonenumber::PhoneNumber;
 
 use crate::service::start_websocket;
 
-#[tokio::main]
+#[actix_rt::main]
 async fn main() {
-    test_registration().await;
     start_websocket().await;
 }
 
-async fn test_registration() {
+async fn _test_registration() {
     println!("Going to register a user");
     let phonenumber = PhoneNumber::from_str("+4917612345678").expect("Invalid phone number");
     let password = "password";
--- backend/src/service/ws.rs
+++ backend/src/service/ws.rs
@@ -1,5 +1,8 @@
+use crate::requests::Result;
 use futures::SinkExt;
 use futures::StreamExt;
+use serde::Deserialize;
+use serde::Serialize;
 use warp::ws::Message;
 use warp::ws::WebSocket;
 
@@ -19,11 +22,49 @@ pub async fn client_connection(mut ws: WebSocket) {
             break;
         }
 
-        let result = ws.send(Message::text("Hello, dear client!")).await;
+        let response = match handle_message(msg).await {
+            Ok(response) => response,
+            Err(e) => {
+                println!("error sending message back: {}", e);
+                Message::text("Invalid message")
+            }
+        };
+
+        let result = ws.send(response).await;
         if let Err(e) = result {
             println!("error sending message back: {}", e);
         }
     }
 
     println!("client disconnected");
+}
+
+async fn handle_message(msg: Message) -> Result<Message> {
+    let req: Request = serde_json::from_str(
+        msg.to_str()
+            .map_err(|_| "Message is not test. Failed to parse.".to_string())?,
+    )
+    .map_err(|e| e.to_string())?;
+
+    println!("Received message: {:#?}", req);
+
+    Ok(Message::text(
+        serde_json::to_string(&Response {
+            id: req.id,
+            msg: "I received your Message :)".to_string(),
+        })
+        .map_err(|_| "Response could not be parsed")?,
+    ))
+}
+
+#[derive(Deserialize, Debug)]
+struct Request {
+    id: u32,
+    msg: String,
+}
+
+#[derive(Serialize)]
+struct Response {
+    id: u32,
+    msg: String,
 }
