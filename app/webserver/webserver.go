021102f447d78d8eb3f7b4ac1b47392eeb991da2
aaron@nanu-c.org <aaron@nan-c.org>
2021-10-26 21:35:45 +0200

    sgnl instead of sgn

--- app/webserver/webserver.go
+++ app/webserver/webserver.go
@@ -282,8 +282,8 @@ func wsReader(conn *websocket.Conn) {
 			json.Unmarshal([]byte(p), &addDeviceMessage)
 			log.Println("[axolotl] add device " + addDeviceMessage.Url)
 			if addDeviceMessage.Url != "" {
-				if strings.Contains(addDeviceMessage.Url, "sgn") {
-					fmt.Printf("found sgn")
+				if strings.Contains(addDeviceMessage.Url, "sgnl") {
+					fmt.Printf("found sgnl")
 					store.AddDevice(addDeviceMessage.Url)
 				}
 			}
