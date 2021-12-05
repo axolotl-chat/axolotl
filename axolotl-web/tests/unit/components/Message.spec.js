acb50d40a66b0b1bdb99d9e35a2787d24101f15b
Thorsten Ploß <thorsten.ploss@wtnet.de>
2021-12-05 18:53:21 +0100

    more test cases and better test configuration
    still not passing

--- axolotl-web/tests/unit/components/Message.spec.js
+++ axolotl-web/tests/unit/components/Message.spec.js
@@ -1,24 +1,82 @@
 import { expect } from 'chai'
+import LinkifyHtml from 'linkifyjs/html'
+import Message from '@/components/Message.vue'
 import { shallowMount } from '@vue/test-utils'
 
-import Message from '@/components/Message.vue'
+const wrapperConfig = {
+  global: {
+    directives: {
+      Translate: function () { }
+    }
+  },
+  methods: {
+    linkify: function (content) {
+      return LinkifyHtml(content);
+    },
+  }
+}
 
 describe('Message.vue', () => {
   it('renders simple message without changes', () => {
     const msg = {
         ID: 'test',
         Message: 'Test Message',
-        Attachment: [],
+        Attachment: '',
         Outgoing: false,
-        QuotedMessage: null
+        QuotedMessage: null,
+        ExpireTimer: 0
     }
     const wrapper = shallowMount(Message, {
+      ...wrapperConfig,
       props: {
         message: msg,
         isGroup: false,
         names: [ ]
       }
     })
-    expect(wrapper.findComponent('.message-text-content').html()).toMatch(msg.Message)
+    console.log(wrapper.html())
+    expect(wrapper.find('.message-text-content').innerHTML).toMatch(msg.Message)
+  })
+
+  it('renders message with link linkified', () => {
+    const expected = 'Visit <a href="axolotl.chat">axolotl.chat</a> if you have time'
+    const msg = {
+        ID: 'test',
+        Message: 'Visit axolotl.chat if you have time',
+        Attachment: '',
+        Outgoing: false,
+        QuotedMessage: null,
+        ExpireTimer: 0
+    }
+    const wrapper = shallowMount(Message, {
+      ...wrapperConfig,
+      props: {
+        message: msg,
+        isGroup: false,
+        names: [ ]
+      }
+    })
+    expect(wrapper.find('.message-text-content').innerHTML).toMatch(expected)
+  })
+
+  it('renders message with html entities escaped', () => {
+    const expected = 'I &lt;3 Axolotl'
+    const msg = {
+        ID: 'test',
+        Message: 'I <3 Axolotl',
+        Attachment: '',
+        Outgoing: false,
+        QuotedMessage: null,
+        ExpireTimer: 0
+    }
+    const wrapper = shallowMount(Message, {
+      ...wrapperConfig,
+      props: {
+        message: msg,
+        isGroup: false,
+        names: [ ]
+      }
+    })
+    expect(wrapper.find('.message-text-content').innerHTML).toMatch(expected)
   })
 })
