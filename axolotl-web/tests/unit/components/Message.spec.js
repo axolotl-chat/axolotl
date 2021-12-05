affe2e9d4dbfcc7ef8d8170b13b6ccb311245a87
Thorsten Ploß <thorsten.ploss@wtnet.de>
2021-12-05 23:50:12 +0100

    try to satisfy deepsource

--- axolotl-web/tests/unit/components/Message.spec.js
+++ axolotl-web/tests/unit/components/Message.spec.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
 import LinkifyHtml from 'linkifyjs/html'
-import Message from '@/components/Message.vue'
 import { mount } from '@vue/test-utils'
+import Message from '@/components/Message.vue'
 
 const wrapperConfig = {
   global: {
