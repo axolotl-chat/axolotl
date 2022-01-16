b6cea459b3be3fe0218a8f5008d1ee77819d291b
Nalysius <anthony.bocci@protonmail.com>
2022-01-16 14:08:20 +0100

    Fix an issue in reading voice notes
    In some situations, voice notes couldn't be read and their duration was
    written as "0s".
    
    The root cause was the attachment's filename. It contained
    a date which may contain slash "/" characters.
    
    The slashes in the filename are now replaced by dashes.
    
    See: /nanu-c/axolotl#713

--- app/store/attachment.go
+++ app/store/attachment.go
@@ -8,6 +8,7 @@ import (
 	"mime"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/nanu-c/axolotl/app/config"
@@ -36,6 +37,7 @@ func SaveAttachment(a *textsecure.Attachment) (Attachment, error) {
 	// 	ext = strings.Replace(a.MimeType, "video/", ".", 1)
 	// }
 	fileName := a.FileName
+	fileName = strings.Replace(fileName, "/", "-", -1)
 	if fileName == "" {
 		fileName = helpers.RandomString(10)
 		extension, err := mime.ExtensionsByType(a.MimeType)
