fc4e03a1e59a49a3ebd986fa93d0ff560934e61e
Olof Nord <olof.nord@tutanota.com>
2022-01-29 23:50:16 +0100

    Specify new flag with flatpak run, clarify wording

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func init() {
 	flag.StringVar(&config.Gui, "e", "", "Specify runtime environment. Use either electron, ut, lorca, qt or server")
 	flag.StringVar(&config.AxolotlWebDir, "axolotlWebDir", "./axolotl-web/dist", "Specify the directory to use for axolotl-web")
 	flag.BoolVar(&config.ElectronDebug, "eDebug", false, "Open electron development console")
-	flag.BoolVar(&config.ElectronBundled, "electronBundled", false, "If true, use bundled (provided) Electron. If false, download Electron on startup")
+	flag.BoolVar(&config.ElectronBundled, "electronBundled", false, "If specified, use bundled (provided) Electron. If not, download Electron on startup")
 	flag.BoolVar(&config.PrintVersion, "version", false, "Print version info")
 	flag.StringVar(&config.ServerHost, "host", "127.0.0.1", "Host to serve UI from.")
 	flag.StringVar(&config.ServerPort, "port", "9080", "Port to serve UI from.")
