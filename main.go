6994aba6ee7710ea2f99a3610a601b16e9a642ba
Olof Nord <olof.nord@tutanota.com>
2022-02-03 20:14:38 +0100

    Undo previous Astielectron window creation edit

--- main.go
+++ main.go
@@ -122,8 +122,7 @@ func runElectron() {
 	center := true
 	height := 800
 	width := 600
-	// TODO: Change back to axolotl-web once Electron starts without problems
-	if w, err = a.NewWindow("https://duckduckgo.com/", &astilectron.WindowOptions{
+	if w, err = a.NewWindow("http://"+config.ServerHost+":"+config.ServerPort, &astilectron.WindowOptions{
 		Center: &center,
 		Height: &height,
 		Width:  &width,
