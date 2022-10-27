93f5814769d96d026f6162faadd933d609fd3422
aaron@nanu-c.org <aaron@nan-c.org>
2022-10-27 22:03:28 +0200

    v1.6.0

--- app/config/config.go
+++ app/config/config.go
@@ -18,7 +18,7 @@ import (
 
 const AppName = "textsecure.nanuc"
 
-const AppVersion = "1.5.1"
+const AppVersion = "1.6.0"
 
 // MaxAttachmentSize does not allow sending attachments larger than 100M for now
 const MaxAttachmentSize int64 = 100 * 1024 * 1024
