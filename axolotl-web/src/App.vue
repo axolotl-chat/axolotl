a5ad672fc9f1542f936743c85efa8f56befec005
aaron@nanu-c.org <aaron@nanu-c.org>
2023-01-26 22:45:02 +0100

    use yarn for building and fix main scrolling

--- axolotl-web/src/App.vue
+++ axolotl-web/src/App.vue
@@ -72,6 +72,7 @@ export default {
 ::-webkit-scrollbar {
   display: none;
 }
+
 html,
 body,
 #app {
@@ -79,27 +80,36 @@ body,
   display: flex;
   flex-direction: column;
 }
+
 body {
   max-height: 100vh;
   overflow: hidden;
 
 }
+
 main {
-  height: calc(
-    100% - 50px
-  ); /* This is needed for Ubuntu Touch because the Blink engine is too old (chrome 61). */
+  height: calc(100% - 50px);
+  /* This is needed for Ubuntu Touch because the Blink engine is too old (chrome 61). */
   overflow: auto;
+}
+
+.main {
+  height: calc(100vh - 50px);
+  overflow-x: scroll;
 }
 
 .btn:focus {
   box-shadow: none;
 }
+
 .btn {
   border-radius: 0px;
 }
+
 .btn-primary {
   background-color: #2090ea;
 }
+
 .no-entries {
   height: 90vh;
   display: flex;
